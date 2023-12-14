4b94b6133d7a52992788a33e5da07f4353968310
Saumyaladdha <136463212+Saumyaladdha@users.noreply.github.com>
2023-12-14 18:52:26 +0530

    Update route.js
    Updating due to error in previous version

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,22 +1,16 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-// Create a Resend instance with your API key
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-// Get the "from" email address from environment variables
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  // Extract email, subject, and message from the request body
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
-
   try {
-    // Send an email using Resend
     const data = await resend.emails.send({
       from: fromEmail,
-      to: [fromEmail, email], // Send to both the sender and the configured "from" email
+      to: [fromEmail, email],
       subject: subject,
       react: (
         <>
@@ -27,11 +21,8 @@ export async function POST(req, res) {
         </>
       ),
     });
-
-    // Return a JSON response with the data from Resend
     return NextResponse.json(data);
   } catch (error) {
-    // If there's an error, return a JSON response with the error
     return NextResponse.json({ error });
   }
 }
